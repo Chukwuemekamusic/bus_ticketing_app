8828a96a8d9329d4d80f57ceec833f09a354afef
Olatunde Yusuf <123118889+olatundey@users.noreply.github.com>
2023-03-02 16:50:23 +0000

    session

--- oldT-user/managebooking.php
+++ oldT-user/managebooking.php
@@ -31,9 +31,9 @@ if (!IsSet($_SESSION["email"]))  //user variable must exist in session to stay h
                     <div class="col-md-10">
                     <nav>
                         <ul class="nav justify-content-end">
-                        <li><a href = "homeUser.php" class="nav-item" >Home</a></li>
-                            <li><a href = "aboutUser.php" class="nav-item">About Us</a></li>
-                            <li><a href = "contactusUser.php" class="nav-item">Contact Us</a></li>   
+                            <li><a href = "home.html" class="nav-item" >Home</a></li>
+                            <li><a href = "about.html" class="nav-item">About Us</a></li>
+                            <li><a href = "contactus.html" class="nav-item">Contact Us</a></li>  
                             <li><a href="profile.php">My Account</a></li> 
                             
                             <li><p>
@@ -57,39 +57,11 @@ if (!IsSet($_SESSION["email"]))  //user variable must exist in session to stay h
 
 
 
-    </main>
-    <footer>
-        <hr>
-        <div class="container">
-            <div class="col-md-12" id="lastleft">
-                <div id="footercontainer" class="row">
-                    <section class="col-md-3">
-                        <!-- <a href="contactus.html"><h4>Contact Us</h4></a> -->
-                        <ul>
-                            <li>Email: info@xxxxbus.com</li>
-                            <li>Phone No.: +44 7498 xxxxxxx</li>
-                            <li>Address: Garthdee, Aberdeen, Scotland, UK</li>
-                        </ul>
-                    </section>
-                    <section class="col-md-6">
-                    </section>
-                    <section class="col-md-3">
-                        <h4>Quick Guide</h4>
-                        <p><a href="faqUser.php">Frequently Asked Question</a></p>      
-                    </section>
-                </div>  
-            </div>
-        </div>   
-        <div id="last">
-            <p>&copy; 2023 Bus Inc. All rights reserved.</p>
-        </div>
-        
-    </footer>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://kit.fontawesome.com/8c47bf12e3.js" crossorigin="anonymous"></script>
+</main>
 </body>
+<footer>
+  &copy; 2023 Bus Booking
+</footer>
 
+</body>
 </html>
