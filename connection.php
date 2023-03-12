f79681e0fe750cb4c258149183de61c4c39e0bfe
emeka <emeka@pop-os.localdomain>
2023-03-12 23:47:59 +0000

    database designed

--- connection.php
+++ connection.php
@@ -1,29 +1,29 @@
 <?php
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "BusApp";
-
-
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// //Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-
-// The LAMP connection 
-$servername = "lamp-mysql8";
+$servername = "localhost";
 $username = "root";
-$password = "tiger";
+$password = "";
 $dbname = "BusApp";
+
+
+// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+// The LAMP connection 
+// $servername = "lamp-mysql8";
+// $username = "root";
+// $password = "tiger";
+// $dbname = "BusApp";
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 
 ?>
