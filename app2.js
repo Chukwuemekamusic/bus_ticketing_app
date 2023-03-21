2d6b768430b087a548951d9d15f714101b4596c9
emeka <emeka@pop-os.localdomain>
2023-03-21 16:16:36 +0000

    js added

---
+++ app2.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const departureDateInput = document.getElementById("departuredate");
+
+
+    // check if the date inputed is either today or ahead
+    departureDateInput.addEventListener("change",function () {
+        let today = new Date();
+        if (new Date(departureDateInput.value) < today.setHours(0, 0, 0, 0)) {
+            alert("Return date must be today or a future date.");
+            departureDateInput.value="";
+        }
+    });
+  
+  })
+  
+  // this function 
+  function addReturnInput() {
+    const element = document.getElementById("returndate");
+  
+    // check if there is any element in the html with class 'return' 
+    if (!element) { 
+      var form = document.getElementById("traveldate");
+    //   form.removeChild(document.getElementById("searchbusbtn"));
+  
+        // create the returndate label    
+      var returnDateLabel = document.createElement("label");
+      returnDateLabel.setAttribute("for", "returndate"); 
+      returnDateLabel.setAttribute("id", "returndatelabel");
+    //   returnDateLabel.innerHTML = "Return Date:";
+      form.appendChild(returnDateLabel);
+      
+      var returnDateInput = document.createElement("input");
+      returnDateInput.setAttribute("type", "date");
+      returnDateInput.setAttribute("id", "returndate");
+      returnDateInput.setAttribute("name", "returndate");
+      form.appendChild(returnDateInput);
+  
+      var emptyspace = document.createElement("br");
+      emptyspace.setAttribute("class", "returnspace");
+      form.appendChild(emptyspace);
+    //   form.appendChild(emptyspace);
+      
+  
+    //   // check return date and display 'Search for Bus' 
+    //   var submitBtn = document.createElement("button");
+    //   submitBtn.setAttribute("type", "submit");
+    //   submitBtn.setAttribute("id", "searchbusbtn");
+    //   submitBtn.innerHTML = "Search for Bus";
+    //   form.appendChild(submitBtn);
+  
+      returnDateInput.addEventListener("change", function () {      
+        let today = new Date();
+        if (new Date(returnDateInput.value) < todaytoday.setHours(0, 0, 0, 0)) {
+            alert("Return date must be today or a future date");
+            returnDateInput.value="";
+        }
+        // TODO need to improve the code to only accept returndate >= departing date
+    });
+  
+  
+    }}
+  
+  function removeReturnInput() {
+    const element = document.getElementById("returndate");
+    
+    if (element) { 
+      element.remove();
+      document.getElementById("returndatelabel").remove();
+      //document.getElementById("searchbusbtn").remove();
+      document.getElementsByClassName("returnspace")[0].remove();
+      document.getElementsByClassName("returnspace")[1].remove();
+      
+  }}
+  
+  
