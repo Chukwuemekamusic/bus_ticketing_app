77f467fab396dba0828dd90853555240b8be814f
emeka <emeka@pop-os.localdomain>
2023-04-10 12:37:18 +0100

    s

--- experiment/admin_populate.php
+++ experiment/admin_populate.php
@@ -13,15 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $seats_booked = 0;
     $bus_number = $_POST["bus_number"];
     
-        // $departure = $busData[0];
-        // $arrival = $busData[1];
-        // $departure_date = date("Y-m-d", strtotime($busData[2]));
-        // $departure_time = date("H:i", strtotime($busData[3]));
-        // $arrival_date = date("Y-m-d", strtotime($busData[4]));
-        // $arrival_time = date("H:i", strtotime($busData[5]));
-        // $bus_capacity = intval($busData[6]);
-        // $seats_booked = 0;
-        // $bus_number = $busData[7];
 
     $sql = "INSERT INTO bus_schedules (departure, arrival, departure_date, departure_time, arrival_date, arrival_time, bus_capacity, bus_number)
     VALUES (:departure, :arrival, :departure_date, :departure_time, :arrival_date, :arrival_time, :bus_capacity, :bus_number)";
@@ -44,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     for ($i = 1; $i <= $bus_capacity; $i++) {
         $seat_number = $i;
         $status = 'available';
-        $stmt = $conn->prepare("INSERT INTO bus_seats (bus_id, seat_number, status)
+        $stmt = $conn->prepare("INSERT INTO bus_seats (bus_schedule_id, seat_number, status)
                 VALUES (:bus_id, :seat_number, :status)");
         $stmt->execute(array(
             ':bus_id' => $bus_id,
